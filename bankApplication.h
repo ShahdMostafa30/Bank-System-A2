6e051bc2d5b40e28658ad32b575b0dfceb506a16
Seif <SE.SeifIbrahim@gmail.com>
2022-11-05 11:12:15 +0200

    Edited bank app files

---
+++ bankApplication.h
@@ -0,0 +1,16 @@
+#ifndef BANKAPPLICATION_H
+#define BANKAPPLICATION_H
+
+#include <iostream>
+#include "bankAccount.h"
+#include "savingsBankAccount.h"
+#include "client.h"
+using namespace std;
+
+class BankAppliaction {
+    private:
+    public:
+    BankAppliaction();
+};
+
+#endif
