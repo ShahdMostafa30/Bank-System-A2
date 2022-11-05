7784e4c049b6c5aeee2f92016ae0b32e444a3c30
Seif <SE.SeifIbrahim@gmail.com>
2022-11-05 11:08:52 +0200

    Edited client

---
+++ client.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <iostream>
+using namespace std;
+
+class Client{
+    private:
+    string name;
+    double minimunBalance;
+    public:
+    Client();
+};
+
+#endif
