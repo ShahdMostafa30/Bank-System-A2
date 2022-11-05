2d1d7da91dbed44c79902523df90519a99493b8f
Seif <SE.SeifIbrahim@gmail.com>
2022-11-05 11:02:05 +0200

    Edited Bank account

---
+++ bankAccount.h
@@ -0,0 +1,15 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+
+#include <iostream>
+using namespace std;
+
+class bankAccount {
+    private:
+    string accountID;
+    double balance;
+    public:
+    bankAccount();
+};
+
+#endif
