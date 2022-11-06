89c06be053d89319b70999f2208fb09f0618d878
Seif <SE.SeifIbrahim@gmail.com>
2022-11-06 18:46:01 +0200

    Edited client.h

--- client.h
+++ client.h
@@ -2,14 +2,23 @@
 #define CLIENT_H
 
 #include <iostream>
+#include "bankAccount.h"
 using namespace std;
 
 class Client{
     private:
     string name;
-    double minimunBalance;
+    string address;
+    string phoneNum;
+    BankAccount *bankAcc;
     public:
     Client();
+    void setName(string n);
+    void setAddress(string a);
+    void setPhoneNum(string p);
+    string getName();
+    string getAddress();
+    string getPhoneNum();
 };
 
 #endif
